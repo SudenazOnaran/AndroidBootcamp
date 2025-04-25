2a13a1dd4d05524bbe36867bbd24dc443e047843
Sudenaz Onaran <128322113+SudenazOnaran@users.noreply.github.com>
2025-04-25 14:19:50 +0300

    Create KotlinDegiskenTanimlama.kt
    Kotlin Dilinde Degisken Tanimlama

---
+++ KotlinDegiskenTanimlama.kt
@@ -0,0 +1,43 @@
+package com.example.kotlinzoomders1.degiskenler
+
+fun main() {
+    val sehir="Izmir"
+    println("Sehir : $sehir")
+
+    val telefon="05316595566"
+    println("Telefon : $telefon")
+
+    val postaKodu = 35  //0... rakam yazılamıyor
+    println("Posta Kodu: $postaKodu")
+
+    val dogumGunu="18.08.2000"
+    println("Dogum Gunu: $dogumGunu")
+
+    val urunYorum="Urunumuzun ozellikleri bunlardir......"
+    println("Urun Yorumu : $urunYorum")
+
+    val indirimOrani=0.30
+    println("Indirim Orani: $indirimOrani")
+
+    val videoSuresi_saniye=5445
+    println("Saniye olarak Video Suresi: $videoSuresi_saniye")
+
+    val dosyaFormatı=".pdf"
+    println("Dosya Formati:$dosyaFormatı")
+
+    val renkKodu="#FF5733"
+    println("Renk Kodu:$renkKodu")
+
+    val ekranBoyutu=2800
+    println("Ekran Boyutu : $ekranBoyutu")
+
+    val ulusalGun="23 Nisan"
+    println(ulusalGun)
+
+    var kalanDakika=30
+    println(kalanDakika)
+
+    var kuponKodu="CXVKDS"
+    println(kuponKodu)
+
+}
